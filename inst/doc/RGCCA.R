b10a26682134966fb398ecda2f8122b8bcf592f7
Arthur Tenenhaus <arthur.tenenhaus@centralesupelec.fr>
2023-10-09 15:40:02 +0000

    version 3.0.2

--- inst/doc/RGCCA.R
+++ inst/doc/RGCCA.R
@@ -19,15 +19,15 @@ knitr::opts_chunk$set(
 RGCCA::available_methods()
 
 ## -----------------------------------------------------------------------------
-library(RGCCA)
-data(Russett)
+library("RGCCA")
+data("Russett")
 colnames(Russett)
 
 ## -----------------------------------------------------------------------------
 A <- list(
-  Agric = Russett[, c("gini", "farm", "rent")],
-  Ind = Russett[, c("gnpr", "labo")],
-  Polit = Russett[, c("inst", "ecks",  "death", "demostab", "dictator")])
+  Agriculture = Russett[, c("gini", "farm", "rent")],
+  Industrial = Russett[, c("gnpr", "labo")],
+  Politic = Russett[, c("inst", "ecks",  "death", "demostab", "dictator")])
 
 lab <- factor(
   apply(Russett[, 9:11], 1, which.max),
@@ -35,7 +35,6 @@ lab <- factor(
 )
 
 ## -----------------------------------------------------------------------------
-#Define the design matrix C.
 C <- matrix(c(0, 0, 1,
               0, 0, 1,
               1, 1, 0), 3, 3)
@@ -52,53 +51,38 @@ fit <- rgcca(blocks = A, connection = C,
              verbose = FALSE)
 
 ## -----------------------------------------------------------------------------
-print(fit)
+summary(fit)
 
-## ---- fig.height = 12, fig.width=18, fig.cap = 'Block-weight vectors of a fitted RGCCA model.', fig.pos = "H"----
+## ----fig-weight, fig.height = 12, fig.width=18, fig.cap = 'Block-weight vectors of a fitted RGCCA model.', fig.pos = "H"----
 plot(fit, type = "weight", block = 1:3, comp = 1,
      display_order = FALSE, cex = 2)
 
-## ---- fig.align='center', fig.cap = '\\label{fig:sample}Graphical display of the countries by drawing the block component of the first block against the block component of the second block, colored according to their political regime.', fig.height = 12, fig.width=18, fig.pos = "H"----
+## ----fig-sample1, fig.align='center', fig.cap = '\\label{fig:sample}Graphical display of the countries by drawing the block component of the first block against the block component of the second block, colored according to their political regime.', fig.height = 12, fig.width=18, fig.pos = "H"----
 plot(fit, type = "sample",
      block = 1:2, comp = 1,
      resp = lab, repel = TRUE, cex = 2)
 
-## ---- fig.align='center', fig.cap = 'Average Variance Explained of the different blocks.', fig.height = 8, fig.width=18, fig.pos = "H"----
+## ----fig-ave, fig.align='center', fig.cap = 'Average variance explained of the different blocks.', fig.height = 8, fig.width=18, fig.pos = "H"----
 plot(fit, type = "ave", cex = 2)
 
-## ---- fig.align='center', fig.cap = 'Correlation circle associated with the first two components of the Agriculture block.', fig.height = 12, fig.width=18, fig.pos = "H"----
+## ----fig-cor-circle, fig.align='center', fig.cap = 'Correlation circle associated with the first two components of the first block.', fig.height = 12, fig.width=18, fig.pos = "H"----
 plot(fit, type = "cor_circle", block = 1, comp = 1:2, 
      display_blocks = 1:3, cex = 2)
 
-## ---- fig.align='center', fig.cap = 'Biplot associated with the first two components ofthe Agriculture block.', fig.height = 12, fig.width=18, fig.pos = "H"----
+## ----fig-biplot1, fig.align='center', fig.cap = 'Biplot associated with the first two components of the first block.', fig.height = 12, fig.width=18, fig.pos = "H"----
 plot(fit, type = "biplot", block = 1, 
      comp = 1:2, repel = TRUE, 
      resp = lab, cex = 2,
      show_arrow = TRUE)
 
-## ---- cache = TRUE, message = FALSE-------------------------------------------
-boot_out <- rgcca_bootstrap(fit, n_boot = 500, n_cores = 1)
-
 ## ---- size = "tiny"-----------------------------------------------------------
-print(boot_out, block = 1:3, ncomp = 1)
+summary(boot_out, block = 1:3, ncomp = 1)
 
-## ---- fig.cap = 'Bootstrap confidence intervals for the block-weight vectors.', fig.height = 12, fig.width=18, fig.pos = "H"----
+## ----fig-boot1, fig.cap = 'Bootstrap confidence intervals for the block-weight vectors.', fig.height = 12, fig.width=18, fig.pos = "H"----
 plot(boot_out, type = "weight", 
      block = 1:3, comp = 1, 
      display_order = FALSE, cex = 2,
      show_stars = TRUE)
-
-## -----------------------------------------------------------------------------
-fit.mcoa <- rgcca(blocks = A, method = "mcoa", ncomp = 2)
-
-## -----------------------------------------------------------------------------
-print(fit.mcoa)
-
-## ---- fig.align='center', fig.cap = 'Biplot of the countries obtained by crossing the two first components of the superblock. Individuals are colored according to their political regime and variables according to their block membership.', fig.height = 12, fig.width=18, fig.pos = "H"----
-plot(fit.mcoa, type = "biplot", 
-     block = 4, comp = 1:2, 
-     response = lab, 
-     repel = TRUE, cex = 2)
 
 ## -----------------------------------------------------------------------------
 fit <- rgcca(blocks = A, connection = C,
@@ -108,7 +92,7 @@ fit <- rgcca(blocks = A, connection = C,
 fit$call$tau
 
 ## -----------------------------------------------------------------------------
-set.seed(123)
+set.seed(0)
 perm_out <- rgcca_permutation(blocks = A, connection = C,
                               par_type = "tau",
                               par_length = 10,
@@ -116,17 +100,27 @@ perm_out <- rgcca_permutation(blocks = A, connection = C,
                               n_perms = 10)
 
 ## ---- width=30----------------------------------------------------------------
-print(perm_out)
+summary(perm_out)
 
-## ---- fig.height = 12, fig.width=18, fig.pos = "H", fig.cap = "Values of the objective function of RGCCA against the sets of tuning parameters, triangles correspond to evaluations on non-permuted datasets."----
+## ----fig-permutation, fig.height = 12, fig.width=18, fig.pos = "H", fig.cap = "Values of the objective function of RGCCA against the sets of tuning parameters, triangles correspond to evaluations on non-permuted datasets."----
 plot(perm_out, cex = 2)
 
 ## -----------------------------------------------------------------------------
 fit <- rgcca(perm_out)
 
+## -----------------------------------------------------------------------------
+fit.mcoa <- rgcca(blocks = A, method = "mcoa", ncomp = 2)
+
+## -----------------------------------------------------------------------------
+summary(fit.mcoa)
+
+## ----fig-biplot2, fig.align='center', fig.cap = 'Biplot of the countries obtained by crossing the two first components of the superblock. Individuals are colored according to their political regime and variables according to their block membership.', fig.height = 12, fig.width=18, fig.pos = "H"----
+plot(fit.mcoa, type = "biplot", 
+     block = 4, comp = 1:2, 
+     response = lab, 
+     repel = TRUE, cex = 2)
+
 ## ----eval = FALSE-------------------------------------------------------------
-#  # Download the dataset's package at http://biodev.cea.fr/sgcca/.
-#  # --> gliomaData_0.4.tar.gz
 #  if (!("gliomaData" %in% rownames(installed.packages()))) {
 #    destfile <- tempfile()
 #    download.file("http://biodev.cea.fr/sgcca/gliomaData_0.4.tar.gz", destfile)
@@ -137,14 +131,13 @@ fit <- rgcca(perm_out)
 knitr::opts_chunk$set(eval = "gliomaData" %in% rownames(installed.packages()))
 
 ## -----------------------------------------------------------------------------
-#  data(ge_cgh_locIGR, package = "gliomaData")
+#  data("ge_cgh_locIGR", package = "gliomaData")
 #  
 #  blocks <- ge_cgh_locIGR$multiblocks
 #  Loc <- factor(ge_cgh_locIGR$y)
 #  levels(Loc) <- colnames(ge_cgh_locIGR$multiblocks$y)
 #  blocks[[3]] <- Loc
 #  
-#  # check dimensions of the blocks
 #  vapply(blocks, NCOL, FUN.VALUE = 1L)
 
 ## -----------------------------------------------------------------------------
@@ -162,7 +155,7 @@ knitr::opts_chunk$set(eval = "gliomaData" %in% rownames(installed.packages()))
 #    rgcca(blocks = blocks, response = 3)
 #  )
 
-## ---- fig.align='center', fig.height = 12, fig.width=18, fig.pos = "H", fig.cap = "Graphical display of the tumors obtained by crossing the block components, and colored according to their location."----
+## ----fig-sample2, fig.align='center', fig.height = 12, fig.width=18, fig.pos = "H", fig.cap = "Graphical display of the tumors obtained by crossing the block components, and colored according to their location."----
 #  plot(fit.rgcca, type = "sample", block = 1:2,
 #       comp = 1, response = Loc, cex = 2)
 
@@ -172,15 +165,15 @@ knitr::opts_chunk$set(eval = "gliomaData" %in% rownames(installed.packages()))
 #                     verbose = FALSE)
 
 ## -----------------------------------------------------------------------------
-#  print(fit.sgcca)
+#  summary(fit.sgcca)
 
 ## ---- cache = TRUE------------------------------------------------------------
-#  set.seed(27) #my favorite number
-#  inTraining <- caret::createDataPartition(
+#  set.seed(0)
+#  in_train <- caret::createDataPartition(
 #    blocks[[3]], p = .75, list = FALSE
 #  )
-#  training <- lapply(blocks, function(x) as.matrix(x)[inTraining, , drop = FALSE])
-#  testing <- lapply(blocks, function(x) as.matrix(x)[-inTraining, , drop = FALSE])
+#  training <- lapply(blocks, function(x) as.matrix(x)[in_train, , drop = FALSE])
+#  testing <- lapply(blocks, function(x) as.matrix(x)[-in_train, , drop = FALSE])
 #  
 #  cv_out <- rgcca_cv(blocks = training, response = 3,
 #                     par_type = "sparsity",
@@ -188,18 +181,18 @@ knitr::opts_chunk$set(eval = "gliomaData" %in% rownames(installed.packages()))
 #                     par_length = 10,
 #                     prediction_model = "lda",
 #                     validation = "kfold",
-#                     k = 3, n_run = 5, metric = "Accuracy",
-#                     n_cores = 2)
+#                     k = 7, n_run = 3, metric = "Balanced_Accuracy",
+#                     n_cores = 1)
 
 ## -----------------------------------------------------------------------------
-#  print(cv_out)
+#  summary(cv_out)
 
-## ---- fig.height = 12, fig.width=18, fig.pos = "H", fig.cap = "Accuracies of the models on the different validation folds against the sets of tuning parameters."----
+## ----fig-cv, fig.height = 12, fig.width=18, fig.pos = "H", fig.cap = "Balanced accuracies of the models on the different validation folds for the different tuning parameter sets."----
 #  plot(cv_out, cex = 2)
 
 ## -----------------------------------------------------------------------------
 #  fit <- rgcca(cv_out)
-#  print(fit)
+#  summary(fit)
 
 ## -----------------------------------------------------------------------------
 #  pred <- rgcca_predict(fit, blocks_test = testing, prediction_model = "lda")
@@ -211,23 +204,19 @@ knitr::opts_chunk$set(eval = "gliomaData" %in% rownames(installed.packages()))
 #  projection <- rgcca_transform(fit, blocks_test = testing)
 
 ## ---- cache = TRUE, message = FALSE-------------------------------------------
+#  set.seed(0)
 #  fit_stab <- rgcca_stability(fit,
 #                              keep = vapply(
 #                                fit$a, function(x) mean(x != 0),
 #                                FUN.VALUE = 1.0
 #                              ),
-#                              n_boot = 100, verbose = TRUE, n_cores = 2)
-
-## ---- fig.height = 12, fig.width=18, fig.align='center', fig.cap = 'Graphical display of the tumors obtained by crossing the components of GE1 and CGH1, and colored according to their location.', fig.pos = "H"----
-#  plot(fit_stab, type = "sample", block = 1:2,
-#       comp = 1, resp = as.character(Loc)[inTraining],
-#       cex = 2
-#       )
+#                              n_boot = 100, verbose = TRUE, n_cores = 1)
 
 ## ---- cache = TRUE------------------------------------------------------------
+#  set.seed(0)
 #  boot_out <- rgcca_bootstrap(fit_stab, n_boot = 500)
 
-## ---- fig.height = 12, fig.width=18, fig.pos = "H", fig.cap = "Bootstrap confidence intervals for the block-weight vectors associated to block GE."----
+## ----fig-boot2, fig.height = 12, fig.width=18, fig.pos = "H", fig.cap = "Bootstrap confidence intervals for the block-weight vectors associated with GE."----
 #  plot(boot_out, block = 1,
 #       display_order = FALSE,
 #       n_mark = 50, cex = 1.5, cex_sub = 17,
